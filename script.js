98441b15d44f72d11794b2988554b436f5654c61
Micheil <fouriemicheil1@gmail.com>
2024-08-19 09:15:06 +0200

    finished js commit

--- script.js
+++ script.js
@@ -10,14 +10,18 @@ fetch("https://cdn.freecodecamp.org/curriculum/news-author-page/authors.json")
     .then((data) => {
         authorDataArr = data;
         displayAuthors(authorDataArr.slice(startingIndex, endingIndex));
-
-
-    }).catch((err) => console.error(`There was an error: ${err}`));
+    }).catch((err) => authorContainer.innerHTML = `<p class="error-msg">There was an error loading the authors</p>`);
 
 const fetchMoreAuthors = () => {
     startingIndex += 8;
     endingIndex += 8;
     displayAuthors(authorDataArr.slice(startingIndex, endingIndex));
+
+    if (authorDataArr.length <= endingIndex) {
+        loadMoreBtn.disabled = true;
+        loadMoreBtn.style.cursor = "not-allowed";
+        loadMoreBtn.textContent = "No more data to load";
+    }
 }
 
 const displayAuthors = (authors) => {
@@ -27,10 +31,13 @@ const displayAuthors = (authors) => {
         <div id="${index}" class="user-card">
             <h2 class="author-name">${author}</h2>
             <img class="user-img" src="${image}" alt="${author} avatar"></img>
-            <p class="bio">${bio}</p>
+            <div class="purple-divider"></div>
+            <p class="bio">${bio.length > 50 ? bio.slice(0, 50) + "..." : bio}</p>
             <a class="author-link" href="${url}" target="_blank">${author}'s author page</a>
         </div>
 
         `;
     });
 }
+
+loadMoreBtn.addEventListener("click", fetchMoreAuthors);

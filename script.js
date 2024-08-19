248621323c4bfabb729250c3fa992a433385c49e
Micheil <fouriemicheil1@gmail.com>
2024-08-19 09:02:41 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+const authorContainer = document.getElementById("author-container");
+const loadMoreBtn = document.getElementById("load-more-btn");
+
+let startingIndex = 0;
+let endingIndex = 8;
+let authorDataArr = [];
+
+fetch("https://cdn.freecodecamp.org/curriculum/news-author-page/authors.json")
+    .then((res) => res.json())
+    .then((data) => {
+        authorDataArr = data;
+        displayAuthors(authorDataArr.slice(startingIndex, endingIndex));
+
+
+    }).catch((err) => console.error(`There was an error: ${err}`));
+
+const fetchMoreAuthors = () => {
+    startingIndex += 8;
+    endingIndex += 8;
+    displayAuthors(authorDataArr.slice(startingIndex, endingIndex));
+}
+
+const displayAuthors = (authors) => {
+    authors.forEach(({ author, image, url, bio }, index) => {
+        authorContainer.innerHTML += `
+
+        <div id="${index}" class="user-card">
+            <h2 class="author-name">${author}</h2>
+            <img class="user-img" src="${image}" alt="${author} avatar"></img>
+            <p class="bio">${bio}</p>
+            <a class="author-link" href="${url}" target="_blank">${author}'s author page</a>
+        </div>
+
+        `;
+    });
+}
